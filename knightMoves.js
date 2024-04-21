15fa7abfcd3c7052bd023fe221c92935a0268529
Hrid1402 <hrid1402@gmail.com>
2024-04-21 11:50:59 -0500

    Finished

--- knightMoves.js
+++ knightMoves.js
@@ -1,53 +1,38 @@
-console.log("Working");
-
-class Square{
-    constructor(data, nextSquare=null){
-        this.data = data;
-        this.nextSquare = nextSquare;
-    };
-}
-class Graph{
-    constructor(){
-        this.list = [];
-    };
-    buildList(array){
-        array.forEach(element => {
-            this.addSquare(element);
-        });
-    };
-    addSquare(element){
-        this.list.push(new Square(element));
+function Node(pos, path) {
+    if (pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7) {
+        return null;
     }
-    getPossibleMoves(positionA){
-        let moves = [];
-        let possibleMoves = [];
-        moves.push([positionA[0]-2,positionA[1]-1]);
-        moves.push([positionA[0]-1,positionA[1]-2]);
-        moves.push([positionA[0]+1,positionA[1]-2]);
-        moves.push([positionA[0]+2,positionA[1]-1]);
-        moves.push([positionA[0]+2,positionA[1]+1]);
-        moves.push([positionA[0]+1,positionA[1]+2]);
-        moves.push([positionA[0]-1,positionA[1]+2]);
-        moves.push([positionA[0]-2,positionA[1]+1]);
-        moves.forEach(M => {
-            if((M[0]>= 0 && M[0]<=7)&&(M[1]>= 0 && M[1]<=7)){
-                possibleMoves.push(M);
+    return { pos, path };
+}
+function knightMoves([x, y], [a, b]){
+    if(a < 0 || a > 7 || b < 0 || b > 7 || x < 0 || x > 7 || y < 0 || y > 7){
+        console.log("Invalid values");
+        return;
+    }
+    let q = [Node([x, y], [[x, y]])];
+    let currentNode = q.shift();
+    while (currentNode.pos[0] !== a || currentNode.pos[1] !== b){
+        let moves = [
+            [currentNode.pos[0] + 2, currentNode.pos[1] - 1],
+            [currentNode.pos[0] + 2, currentNode.pos[1] + 1],
+            [currentNode.pos[0] - 2, currentNode.pos[1] - 1],
+            [currentNode.pos[0] - 2, currentNode.pos[1] + 1],
+            [currentNode.pos[0] + 1, currentNode.pos[1] - 2],
+            [currentNode.pos[0] + 1, currentNode.pos[1] + 2],
+            [currentNode.pos[0] - 1, currentNode.pos[1] - 2],
+            [currentNode.pos[0] - 1, currentNode.pos[1] + 2]
+        ];
+        moves.forEach((move) => {
+            let node = Node(move, currentNode.path.concat([move]));
+            if (node) {
+                q.push(node);
             }
         });
-        return possibleMoves;
+        currentNode = q.shift();
     }
-    getShortPath(positionA, positionB){
-        const PM = this.getPossibleMoves(positionA);
-        this.buildList(PM);
-        for(let i = 0; i < this.list.length; i++){
-            this.list[i].nextSquare = this.getPossibleMoves(this.list[i].data);
-            console.log(this.getPossibleMoves(this.list[i].data));
-        }
-    }
+    console.log(`=> You made it in ${currentNode.path.length - 1} moves!  Here's your path:`);
+    currentNode.path.forEach((pos) => {
+        console.log(pos);
+    });
 }
-const myGraph = new Graph;
-
-//let moves = myGraph.getPossibleMoves([3,3]);
-//myGraph.buildList(moves);
-myGraph.getShortPath([3,3], [2,5]);
-console.log(myGraph.list);
+knightMoves([3,3], [7,7]);
