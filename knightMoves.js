8c734028406d9d5709a636ce2855a4dde072d8c7
Hrid1402 <hrid1402@gmail.com>
2024-04-21 11:16:14 -0500

    Works

--- knightMoves.js
+++ knightMoves.js
@@ -1,39 +1,53 @@
 console.log("Working");
-let board = [
-    [0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 7, 8, 9, 0, 0, 0,],
-    [0, 0, 1, 2, 3, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 5, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0,]
-];
-function knightMoves(positionA, positionB){
-    console.log("PosA:",positionA);
-    console.log("PosB:",positionB);
-    let moves = [];
-    let possibleMoves = [];
-    moves.push([positionA[0]-2,positionA[1]-1]);
-    moves.push([positionA[0]-1,positionA[1]-2]);
 
-    moves.push([positionA[0]+1,positionA[1]-2]);
-    moves.push([positionA[0]+2,positionA[1]-1]);
-
-    moves.push([positionA[0]+2,positionA[1]+1]);
-    moves.push([positionA[0]+1,positionA[1]+2]);
-
-    moves.push([positionA[0]-1,positionA[1]+2]);
-    moves.push([positionA[0]-2,positionA[1]+1]);
-
-    moves.forEach(M => {
-        //console.log(M);
-        if((M[0]>= 0 && M[0]<=7)&&(M[1]>= 0 && M[1]<=7)){
-            possibleMoves.push(M);
-        }
-    });
-    console.log("---------");
-    return possibleMoves;
+class Square{
+    constructor(data, nextSquare=null){
+        this.data = data;
+        this.nextSquare = nextSquare;
+    };
 }
-let moves = knightMoves([4,1], [2,5]);
-console.log(moves);
+class Graph{
+    constructor(){
+        this.list = [];
+    };
+    buildList(array){
+        array.forEach(element => {
+            this.addSquare(element);
+        });
+    };
+    addSquare(element){
+        this.list.push(new Square(element));
+    }
+    getPossibleMoves(positionA){
+        let moves = [];
+        let possibleMoves = [];
+        moves.push([positionA[0]-2,positionA[1]-1]);
+        moves.push([positionA[0]-1,positionA[1]-2]);
+        moves.push([positionA[0]+1,positionA[1]-2]);
+        moves.push([positionA[0]+2,positionA[1]-1]);
+        moves.push([positionA[0]+2,positionA[1]+1]);
+        moves.push([positionA[0]+1,positionA[1]+2]);
+        moves.push([positionA[0]-1,positionA[1]+2]);
+        moves.push([positionA[0]-2,positionA[1]+1]);
+        moves.forEach(M => {
+            if((M[0]>= 0 && M[0]<=7)&&(M[1]>= 0 && M[1]<=7)){
+                possibleMoves.push(M);
+            }
+        });
+        return possibleMoves;
+    }
+    getShortPath(positionA, positionB){
+        const PM = this.getPossibleMoves(positionA);
+        this.buildList(PM);
+        for(let i = 0; i < this.list.length; i++){
+            this.list[i].nextSquare = this.getPossibleMoves(this.list[i].data);
+            console.log(this.getPossibleMoves(this.list[i].data));
+        }
+    }
+}
+const myGraph = new Graph;
+
+//let moves = myGraph.getPossibleMoves([3,3]);
+//myGraph.buildList(moves);
+myGraph.getShortPath([3,3], [2,5]);
+console.log(myGraph.list);
